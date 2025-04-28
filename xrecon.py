7bd2c1d75e1e5811a6af1124962c366d8f1f4648
Amel1308 <69460548+Amel1308@users.noreply.github.com>
2025-04-28 07:29:16 -0400

    Update and rename crawler_recon.py to xrecon.py

--- xrecon.py
+++ xrecon.py
@@ -1,3 +1,4 @@
+import argparse
 from logo import print_logo
 from parser import parse_html
 from downloader import download_html, download_scripts
@@ -5,18 +6,27 @@ from output_manager import save_outputs
 from js_extractor import extract_endpoints
 from recon_manager import clean_and_merge_outputs
 
+def get_target():
+    parser = argparse.ArgumentParser(description="XRecon - Fast and modular web reconnaissance tool.")
+    parser.add_argument("-d", "--domain", help="Target domain to scan", required=False)
+    args = parser.parse_args()
+
+    if args.domain:
+        return args.domain
+    else:
+        return input("Enter target domain: ")
+
 def main():
     print_logo()
 
-
-     # 1. Get the target URL from the user
-    target_url = input("Enter the target URL: ").strip()
+    # 1. Get the target URL
+    target = get_target()
 
     # 2. Download the HTML content
-    html_content = download_html(target_url)
+    html_content = download_html(target)
 
     # 3. Parse HTML to extract links, forms, hidden inputs, and script sources
-    links, forms, hidden_inputs, script_sources = parse_html(html_content, target_url)
+    links, forms, hidden_inputs, script_sources = parse_html(html_content, target)
 
     # 4. Save all extracted data to the outputs folder
     save_outputs(links, forms, hidden_inputs, script_sources, output_dir="./outputs")
@@ -30,9 +40,9 @@ def main():
     # 7. Clean and merge all outputs into final files
     clean_and_merge_outputs("./outputs")
 
-
 if __name__ == "__main__":
     main()
+
 
 
 
